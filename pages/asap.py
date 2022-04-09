8ef85f30f1bbb3d5563d56198d3a374a41505517
Sidharrth Nagappan <sidharrth2002@gmail.com>
2022-04-09 11:44:07 +0800

    asap heatmaps work

--- pages/asap.py
+++ pages/asap.py
@@ -15,20 +15,6 @@ def app():
                 Here, you can evaluate the scoring framework on the ASAP-AES scoring dataset. Select an essay prompt
                 below and the system will return the predicted score.
                 """)
-    
-    fig, ax = plt.subplots()
-    # attentions = requests.get(API_URL + '/word-level-attention').json()
-    # fig = px.imshow(np.array(attentions[0][0])[:2,:65], width=500, height=2000)
-    # fig = go.Figure(data=go.Heatmap(
-    #                 z=np.array(attentions[0][0])[:2,:65]))
-    # fig.layout.height = 400
-    # fig.layout.width = 600
-    # fig.update(layout_coloraxis_showscale=False)
-    # fig.layout.height = 600
-    # fig.layout.width = 800
-    # st.write(fig)
-    # ax = sns.heatmap(attentions[0][0], cmap='viridis', xticklabels=False, yticklabels=False, ax=ax)
-    # st.write(fig)
 
     essay_set = st.radio("Select the essay set", ["Set 3", "Set 4", "Set 5", "Set 6"])
 
@@ -39,6 +25,25 @@ def app():
     if submit:
         with st.spinner('Working'):
             score = requests.post(f"{API_URL}/predict-asap-aes/", json={"text": response, "essay_set": "".join(essay_set.lower().split())}).json()
-            st.write(score)
+            st.success(f"Your score is: {str(score['eval_score'])} / 4")
+            chart = st.progress((score['eval_score']/4) * 1)
             if score['eval_score'] >= 3:
                 st.balloons()
+
+        fig, ax = plt.subplots()
+        attentions = requests.get(API_URL + '/word-level-attention').json()
+        print(attentions)
+
+        key_phrase = st.selectbox("Select a key phrase to view attention heatmap", score["keywords"])
+
+        key_phrase_index = score["keywords"].index(key_phrase)
+        # fig = px.imshow(np.array(attentions[0][key_phrase_index])[:len(key_phrase.split()),:len(score['text'].split())], width=500, height=2000)
+        fig = go.Figure(data=go.Heatmap(z=np.array(attentions[0][key_phrase_index])[:len(key_phrase.split()),:len(score['text'].split())], x=score['text'].split(), y=key_phrase.split()))
+        # fig.layout.height = 400
+        # fig.layout.width = 600
+        fig.update(layout_coloraxis_showscale=False)
+        fig.layout.height = 600
+        fig.layout.width = 800
+        st.write(fig)
+            # ax = sns.heatmap(attentions[0][0], cmap='viridis', xticklabels=False, yticklabels=False, ax=ax)
+            # st.write(fig)
